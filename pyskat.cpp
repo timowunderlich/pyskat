65922fbeeaf851ecd4d2df4ebce69653d1f47bb8
Timo Wunderlich <timo.wunderlich@kip.uni-heidelberg.de>
2018-11-21 18:00:42 +0100

    Add python bindings

---
+++ pyskat.cpp
@@ -0,0 +1,35 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/operators.h>
+#include <Python.h>
+
+#include "halfskat.hpp"
+#include "cards.hpp"
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(pyskat, m) {
+    py::enum_<Cards::Color>(m, "Color")
+        .value("Diamonds", Cards::Color::Diamonds)
+        .value("Hearts", Cards::Color::Hearts)
+        .value("Spades", Cards::Color::Spades)
+        .value("Clubs", Cards::Color::Clubs);
+    py::enum_<Cards::Rank>(m, "Rank", py::arithmetic())
+        .value("Seven", Cards::Rank::Seven)
+        .value("Eight", Cards::Rank::Eight)
+        .value("Nine", Cards::Rank::Nine)
+        .value("Queen", Cards::Rank::Queen)
+        .value("King", Cards::Rank::King)
+        .value("Ten", Cards::Rank::Ten)
+        .value("Ace", Cards::Rank::Ace)
+        .value("Jack", Cards::Rank::Jack);
+    py::class_<Cards::Card>(m, "Card")
+        .def(py::init<>())
+        .def(py::init<Cards::Color const, Cards::Rank const>())
+        .def(py::self == py::self)
+        .def_readwrite("color", &Cards::Card::color)
+        .def("__str__", [](Cards::Card const& c) { 
+            std::stringstream ss;
+            ss << c; 
+            return ss.str(); })
+        .def_readwrite("rank", &Cards::Card::rank);
+}
