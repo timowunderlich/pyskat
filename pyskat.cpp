1a0903b9122d8df3cfebe712bd3da60013f9fd2e
Timo Wunderlich <timo.wunderlich@kip.uni-heidelberg.de>
2018-11-23 22:46:23 +0100

    Add python bindings for HalfSkat::Game

--- pyskat.cpp
+++ pyskat.cpp
@@ -10,6 +10,7 @@
 namespace py = pybind11;
 
 PYBIND11_MODULE(pyskat, m) {
+    // Cards bindings
     py::enum_<Cards::Color>(m, "Color")
         .value("Diamonds", Cards::Color::Diamonds)
         .value("Hearts", Cards::Color::Hearts)
@@ -42,4 +43,28 @@ PYBIND11_MODULE(pyskat, m) {
     m.def("get_card_points", &Cards::get_card_points);
     m.def("get_suit_base_value", (int (*)(Cards::Card const&)) &Cards::get_suit_base_value);
     m.def("get_suit_base_value", (int (*)(Cards::Color const&)) &Cards::get_suit_base_value);
+
+    // HalfSkat bindings
+    py::class_<HalfSkat::Player, std::shared_ptr<HalfSkat::Player>>(m, "Player")
+        .def(py::init<>())
+        .def("get_action", &HalfSkat::Player::get_action);
+    py::class_<HalfSkat::RandomPlayer, std::shared_ptr<HalfSkat::RandomPlayer>>(m, "RandomPlayer")
+        .def(py::init<>())
+        .def("get_action", &HalfSkat::Player::get_action);
+    py::class_<HalfSkat::HumanPlayer, std::shared_ptr<HalfSkat::HumanPlayer>>(m, "HumanPlayer")
+        .def(py::init<>())
+        .def("get_action", &HalfSkat::Player::get_action);
+    py::class_<HalfSkat::Game>(m, "Game")
+        .def(py::init<int const>(), py::arg("max_rounds") = 1000)
+        .def(py::init<std::shared_ptr<HalfSkat::Player>, int const>(), py::arg("first_player"), py::arg("max_rounds") = 1000)
+        .def(py::init<std::shared_ptr<HalfSkat::RandomPlayer>, int const>(), py::arg("first_player"), py::arg("max_rounds") = 1000)
+        .def(py::init<std::shared_ptr<HalfSkat::HumanPlayer>, int const>(), py::arg("first_player"), py::arg("max_rounds") = 1000)
+        .def("step_by_trick", &HalfSkat::Game::step_by_trick)
+        .def("step_by_round", &HalfSkat::Game::step_by_round)
+        .def("run_whole_game", &HalfSkat::Game::run_whole_game)
+        .def("get_trick", &HalfSkat::Game::get_trick)
+        .def("get_points", &HalfSkat::Game::get_points)
+        .def("get_round", &HalfSkat::Game::get_round)
+        .def("get_max_rounds", &HalfSkat::Game::get_max_rounds)
+        .def_readonly("trump", &HalfSkat::Game::trump);
 }
